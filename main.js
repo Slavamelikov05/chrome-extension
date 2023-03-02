9cb94d06dac1e86b8ba928409f566e6c109b82ab
Jonas <jonas.gantar@gmail.com>
2023-03-01 17:22:40 -0800

    hi

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 
+function emojify() {
+    // Replace all text on the page with emojis
+    const allNodes = document.getElementsByTagName("*");
+  
+  // Loop through all nodes
+  for (let i = 0; i < allNodes.length; i++) {
+    const node = allNodes[i];
+    // Check if node is a text node
+    if (node.nodeType === Node.TEXT_NODE) {
+      // Replace the text with the emoji
+      node.textContent = "😊👍🎉";
+    }
+  }
+
+  }
+
+  let emojifyButton =  document.getElementById("emojify")
+
+  emojifyButton.addEventListener("click",emojify)
